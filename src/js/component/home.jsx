e12c05af49a45b6f639d7096a7726b560b3c403f
UkuJoeMP <josemipinton@gmail.com>
2024-12-27 21:14:28 +0000

    Todo List

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+export const Home = () => {
+	const [todos, setTodos] = useState([]);
+	const [taskInput, setTaskInput] = useState('');
+  
+	const handleFormSubmit = (e) => {
+	  e.preventDefault();
+	  setTodos([
+		...todos,
+		{title: taskInput}
+	  ]);
+	  setTaskInput('');
+	};
+  
+	const deleteTask = (taskId) => {
+	  setTodos(todos.filter(task => task.id !== taskId));
+	};
+  
+	const tasksToRender = todos.map(task => (
+	  <li key={task.id}>
+		<div className="view">
+		  <label>{task.title}</label>
+		  <button className="destroy" onClick={() => deleteTask(task.id)}></button>
 		</div>
+	  </li>
+	));
+  
+	return (
+	  <section className="todoapp">
+		<header className="header">
+		  <h1>todos</h1>
+		  <form onSubmit={handleFormSubmit}>
+			<input
+			  autoFocus={true}
+			  className="new-todo"
+			  placeholder="What needs to be done?"
+			  value={taskInput}
+			  onChange={(e) => setTaskInput(e.target.value)}
+			/>
+		  </form>
+		</header>
+		<section className="main">
+		  <ul className="todo-list">
+			{tasksToRender}
+		  </ul>
+		</section>
+		<footer className="footer">
+		  <span className="todo-count">
+			<strong>{todos.filter(task => !task.done).length}</strong> item left
+		  </span>
+		</footer>
+	  </section>
 	);
-};
-
-export default Home;
+  };
+  export default Home;
